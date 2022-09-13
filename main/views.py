6929c1a7e6dfa192e08b5796aab07881bd3440ab
akondorsky@gmail.com <akondorsky>
2022-09-13 19:02:42 +0200

    Release code chapter 35

--- main/views.py
+++ main/views.py
@@ -17,8 +17,10 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 
 
-from .models import AdvUser, SubRubric, Bb
-from .forms import ChangeUserInfoForm, RegisterUserForm, SearchForm, BbForm, AIFormSet
+from .models import AdvUser, SubRubric, Bb, Comment
+from .forms import ChangeUserInfoForm, RegisterUserForm, \
+                   SearchForm, BbForm, AIFormSet, \
+                   UserCommentForm, GuestCommentForm
 
 
 # Create your views here.
@@ -138,9 +140,27 @@ def by_rubric(request, pk):
 
 
 def detail(request, rubric_pk, pk):
-    bb = get_object_or_404(Bb, pk=pk)
+    bb = Bb.objects.get(pk=pk)
     ais = bb.additionalimage_set.all()
-    context = {'bb': bb, 'ais': ais}
+    comments = Comment.objects.filter(bb=pk, is_active=True)
+    initial = {'bb': bb.pk}
+    if request.user.is_authenticated:
+        initial['author'] = request.user.username
+        form_class = UserCommentForm
+    else:
+        form_class = GuestCommentForm
+    form = form_class(initial=initial)
+    if request.method == 'POST':
+        c_form = form_class(request.POST)
+        if c_form.is_valid():
+            c_form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Комментарий добавлен')
+        else:
+            form = c_form
+            messages.add_message(request, messages.WARNING,
+                                 'Комментарий не добавлен')
+    context = {'bb': bb, 'ais': ais, 'comments': comments, 'form': form}
     return render(request, 'main/detail.html', context)
 
 
@@ -193,3 +213,6 @@ def profile_bb_delete(request, pk):
     else:
         context = {'bb': bb}
         return render(request, 'main/profile_bb_delete.html', context)
+
+
+
